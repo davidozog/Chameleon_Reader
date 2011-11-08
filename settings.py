f11e2b6a0590cc7907eb04e54492f9bb3d63fb41
David Ozog <gozo311@gmail.com>
2011-11-08 15:49:32 -0800

    small fix - GameReader to Chameleon_Reader

--- settings.py
+++ settings.py
@@ -18,10 +18,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'gamereader',                      # Or path to database file if using sqlite3.
         'USER': USER,                      # Not used with sqlite3.
-        'PASSWORD': 'PostGres311',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
