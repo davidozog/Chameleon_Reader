ac528ee17dc7dc1beb9d1d1e51202459e7ba70b5
David Ozog <thedozog@gmail.com>
2011-11-12 16:34:49 -0800

    adjusting the mod_wdgi file

--- apache/django.py
+++ apache/django.py
@@ -1,14 +1,14 @@
 import os
 import sys
 
-path1 = '/home/dave/School/CIS522/cis522f11_2g/trunk/GameReader/'
-path2 = '/home/dave/School/CIS522/cis522f11_2g/trunk/'
+path1 = '/home/dave/School/CIS522/'
+path2 = '/home/dave/School/CIS522/Chameleon_Reader/'
 sys.path.append(path1)
 sys.path.append(path2)
 
 print sys.path
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'GameReader.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Chameleon_Reader.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
